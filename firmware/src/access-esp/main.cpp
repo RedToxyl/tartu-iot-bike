3f8780d34b79a772281c1c052498f6bc652dcebb
tw <wolf19@uni-potsdam.de>
2026-05-20 14:33:01 +0300

    fix: access-esp compiles

--- firmware/src/access-esp/main.cpp
+++ firmware/src/access-esp/main.cpp
@@ -20,9 +20,13 @@ struct Space{
     bool bikePresent;
     int last_access_attempt_time;
     String last_access_attempt_rfid;
-}
+};
 
-String stationId = STATION_ID
+String stationId;
+
+void validate_access(char *topic, char *payload);
+void update_space(char *topic, char *payload);
+void attempt_access(char *topic, char *payload);
 
 Space spaces[N_SPACES];
 
@@ -34,18 +38,17 @@ void initializeSpaces()
     for (int i = 0; i < N_SPACES; i++)
     {
         String spaceId = String(i);
-
-
-        ECL::publish("api/create_space", "\"station\": \"" + stationId + "\", \"space\": \"" + spaceId + "\"");
+        String createSpacePayload = "\"station\": \"" + stationId + "\", \"space\": \"" + spaceId + "\"";
+        ECL::publish("api/create_space", createSpacePayload.c_str());
         spaces[i] = {spaceId, false, 0, ""};
     }
 }
 
-void initalizeHandlers(){
+void initializeHandlers(){
     ECL::subscribe("api/return/#", validate_access);
     ECL::subscribe("space/#/bike", update_space);
     ECL::subscribe("space/#/rfid", attempt_access);
-}
+};
 
 void setup()
 {
@@ -58,9 +61,9 @@ void setup()
     ECL::log.printf(
         "My MAC address is [%s]\n",
         stationId.c_str());
-    ECL::subscribe("space");
-    ECL::subscribe("api")
-    ECL::publish('api/create_station', "\"station\": \"" + stationId + "\", \"name\": \"" + STATION_NAME + "\", \"lat\": " + String(LATITUDE) + ", \"lon\": " + String(LONGITUDE) + "\"");
+    initializeHandlers();
+    String createStationPayload = "\"station\": \"" + stationId + "\", \"name\": \"" + STATION_NAME + "\", \"lat\": " + String(LATITUDE) + ", \"lon\": " + String(LONGITUDE) + "\"";
+    ECL::publish("api/create_station", createStationPayload.c_str());
     initializeSpaces();
 }
 
@@ -71,7 +74,7 @@ void setup()
 void loop()
 {
     ECL::loop();
-    ECL::publish('api/keep_alive', stationId.c_str());
+    ECL::publish("api/keep_alive", stationId.c_str());
 
     delay(1000);
 }
@@ -103,39 +106,41 @@ int topicToSpaceId(const char *topic)
 // =========================
 void allow_access(int spaceId){ // called when a valid rfid was presented and the server responded in time
     String spaceStr = String(spaceId);
-    // unlock
-    ECL::publish(
-        "space/" + spaceStr + "/solenoid",
-        "unlock");
+    String unlockTopic = "space/" + spaceStr + "/solenoid";
+    ECL::publish(unlockTopic.c_str(), "unlock");
 
     delay(BIKE_TAKEOUT_TIME);
 
-    // lock
-    ECL::publish(
-        "space/" + spaceStr + "/solenoid",
-        "lock");
+    String lockTopic = "space/" + spaceStr + "/solenoid";
+    ECL::publish(lockTopic.c_str(), "lock");
 
     // update backend
+    String attemptRfid = "";
+    if (spaceId >= 0 && spaceId < N_SPACES)
+    {
+        attemptRfid = spaces[spaceId].last_access_attempt_rfid;
+    }
+
+    String payload = "\"station\": \"" + stationId + "\", \"space\": \"" + spaceStr + "\", \"rfid\": \"" + attemptRfid + "\"";
     if (spaceId >= 0 &&
         spaceId < N_SPACES &&
         spaces[spaceId].bikePresent) // we're doing it this way so that bike-detection has a purpose
     {
-        ECL::publish('api/lock', "\"station\": \"" + stationId + "\", \"space\": \"" + spaceStr + "\", \"rfid\": \"" + rfid + "\"");
+        ECL::publish("api/lock", payload.c_str());
         ECL::log.println(
             "Bike detected -> locked");
     }
     else
     {
-        ECL::publish('api/unlock', "\"station\": \"" + stationId + "\", \"space\": \"" + spaceStr + "\", \"rfid\": \"" + rfid + "\"");
+        ECL::publish("api/unlock", payload.c_str());
         ECL::log.println(
             "No bike -> unlocked");
     }
 }
 
-
 void validate_access(char *topic, char *payload) // called when the server responds with the stored rfid for a space
 {
-    JsonDocument spaceState;
+    DynamicJsonDocument spaceState(256);
     deserializeJson(spaceState, payload);
 
     int spaceId = spaceState["s.id"].as<int>();
@@ -166,6 +171,7 @@ void attempt_access(char *topic, char *payload) // called when the user presents
 {
     int spaceId = topicToSpaceId(topic);
     String rfid(payload);
+    String spaceStr = String(spaceId);
 
     if (spaceId >= 0 && spaceId < N_SPACES)
     {
@@ -173,7 +179,8 @@ void attempt_access(char *topic, char *payload) // called when the user presents
         spaces[spaceId].last_access_attempt_rfid = String(payload);
     }
  
-    ECL::publish('api/space', "\"station\": \"" + stationId + "\", \"space\": \"" + spaceStr + "\"");
+    String payloadStr = "\"station\": \"" + stationId + "\", \"space\": \"" + spaceStr + "\"";
+    ECL::publish("api/space", payloadStr.c_str());
 
     ECL::log.printf(
         "Request for space %d from RFID %s\n",
