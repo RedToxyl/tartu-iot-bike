65425d44a1a08fb4f68b4911a3ea3509fd5fb0a0
Egor Lukyanenko <egor.lukyanenko@gmail.com>
2026-05-24 14:08:58 +0300

    master example update

--- firmware/src/examples/master.cpp
+++ firmware/src/examples/master.cpp
@@ -2,7 +2,6 @@
 #define ECL_HARDWARE_SERIAL_RX 16
 #define ECL_HARDWARE_SERIAL_TX 17
 #define ECL_HARDWARE_SERIAL_SPEED 9600
-// #define ECL_HARDWARE_SERIAL_ENABLE_LOGS
 
 #define ECL_ESPNOW_ENABLE
 
@@ -10,11 +9,14 @@
 #include <map>
 
 std::map<String, String> state;
+String stationId;
 
 void eventCallback(char *topic, char *payload)
 {
     state[String(topic)] = String(payload);
+
     ECL::log.printf("[EVENT] %s %s\n", topic, payload);
+
     hardwareSerial.print(topic);
     hardwareSerial.print("|");
     hardwareSerial.println(payload);
@@ -29,19 +31,29 @@ void handleConnection()
 
         if (incoming == "SYNC")
         {
-            ECL::log.println("Sending full state sync...");
+            ECL::log.println("SYNC requested: sending state...");
 
             for (const auto &entry : state)
             {
-                const auto &savedTopic = entry.first;
-                const auto &savedPayload = entry.second;
-
-                hardwareSerial.print(savedTopic);
+                hardwareSerial.print(entry.first);
                 hardwareSerial.print("|");
-                hardwareSerial.println(savedPayload);
+                hardwareSerial.println(entry.second);
             }
 
-            ECL::log.println("SYSTEM|SYNC_COMPLETE");
+            hardwareSerial.println("SYSTEM|SYNC_COMPLETE");
+            ECL::log.println("SYNC compleate!");
+        }
+        else if (incoming.indexOf('|') != -1)
+        {
+            int splitIdx = incoming.indexOf('|');
+            String targetTopic = incoming.substring(0, splitIdx);
+            String targetPayload = incoming.substring(splitIdx + 1);
+
+            state[targetTopic] = targetPayload;
+
+            ECL::publish(targetTopic.c_str(), targetPayload.c_str());
+
+            ECL::log.printf("[FORWARD TO MESH] %s -> %s\n", targetTopic.c_str(), targetPayload.c_str());
         }
     }
 }
@@ -50,6 +62,10 @@ void setup()
 {
     ECL::begin();
     ECL::subscribe("#", eventCallback);
+    stationId = WiFi.macAddress();
+    state["system/mac"] = stationId;
+
+    ECL::log.printf("Master Bridge Ready. MAC: %s\n", stationId.c_str());
 }
 
 void loop()
